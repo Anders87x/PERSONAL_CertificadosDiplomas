1fdf5efa9ddaa9b7ffc70564c98e06c93183fd9e
AnderCode <davisanderson87@gmail.com>
2022-06-06 21:52:12 -0500

    06/06/22 Subiendo informacion desde excel

--- view/AdminMntUsuario/adminmntusuario.js
+++ view/AdminMntUsuario/adminmntusuario.js
@@ -138,4 +138,49 @@ function nuevo(){
     $('#modalmantenimiento').modal('show');
 }
 
+$(document).on("click", "#btnplantilla", function () {
+    $('#modalplantilla').modal('show');
+});
+
+var ExcelToJSON = function() {
+    this.parseExcel = function(file) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+            //TODO: Recorrido a todas las pestañas
+            workbook.SheetNames.forEach(function(sheetName) {
+                // Here is your object
+                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                var json_object = JSON.stringify(XL_row_object);
+                UserList = JSON.parse(json_object);
+
+                console.log(UserList)
+                for (i = 0; i < productList.length; i++) {
+
+                    var columns = Object.values(UserList[i])
+
+                }
+
+            })
+        };
+        reader.onerror = function(ex) {
+            console.log(ex);
+        };
+
+        reader.readAsBinaryString(file);
+    };
+};
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+    var xl2json = new ExcelToJSON();
+    xl2json.parseExcel(files[0]);
+}
+
+document.getElementById('upload').addEventListener('change', handleFileSelect, false);
+
 init();
